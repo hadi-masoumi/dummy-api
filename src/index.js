9bbdf193562b76d89041c959e867b61ddaf28331
Hadi Masoumi <30729984+hadimasoumi@users.noreply.github.com>
2024-11-14 22:30:59 +0300

    fix error

--- src/index.js
+++ src/index.js
@@ -8,19 +8,16 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'jade');
 
-//post route for adding new task
 app.get("/users", function (req, res) {
   setTimeout(() => {
     res.send(usersJSON);
   }, 2000);
 });
 
-//render the ejs and display added task, completed task
 app.get("/", function (req, res) {
-  res.render("index", {task: task, complete: complete});
+  res.send('This is a dummy api');
 });
 
-//set app to listen on port 3000
 app.listen(3000, function () {
   console.log("server is running on port http://localhost:" + port);
 });
